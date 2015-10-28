78659e2ed64e56ef86c433653e60d707dd752335
Erik Kristensen <erik@erikkristensen.com>
2015-10-28 11:50:27 -0600

    code be born :sunrise:

---
+++ README.md
@@ -0,0 +1,45 @@
+**STATUS: WIP -- Mostly implemented minus the summary metric**
+
+# Prometheus Client (Pure Javascript)
+
+Originally based https://github.com/StreamMachine/prometheus_client_nodejs, but written without CoffeScript in just JavaScript. (Originally Licensed under Apache 2.0)
+
+[Prometheus](http://prometheus.io) instrumentation metrics library for Node.JS. Metrics are intended to be scraped by a Prometheus server.
+
+## Usage
+
+### Getting Started
+
+Install the `prometheus-client` package with NPM:
+
+    npm install prometheus-client-js
+
+Then require the package and set up a new client instance:
+
+```javascript
+var Prometheus = require('prometheus-client-js')
+var client = new Prometheus()
+```
+
+The client library can create an HTTP app to serve up your metrics, or you
+can point to the output function from your own app router.
+
+To use the built-in server:
+
+```javascript
+client.createServer(9090)
+```
+
+Then use `curl http://127.0.0.1:9090` to see a text representation of your metrics.
+
+By default, the Prometheus client will use a global namespace. That means that
+any metrics registered inside your app (even by libraries) will show up in your
+client without any need to pass around a client object.
+
+
+## Metric Types
+
+1. Counter (http://prometheus.io/docs/concepts/metric_types/#counter)
+2. Gauge (http://prometheus.io/docs/concepts/metric_types/#gauge)
+3. Histogram (http://prometheus.io/docs/concepts/metric_types/#histogram)
+4. Summary (http://prometheus.io/docs/concepts/metric_types/#summary) (NOT YET IMPLEMENTED)
---
+++ test.js
@@ -0,0 +1,92 @@
+var test = require('tape')
+
+var counterRand = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+var gaugeRand = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+var histogramRand = Math.floor(Math.random() * (20 - 1 + 1) + 1)
+
+test('counter (random ' + counterRand + ')', function(t) {
+  var Prometheus = require("./lib/client")
+  var client = new Prometheus()
+
+  var counter = client.newCounter({
+    namespace: "counter_test",
+    name: "elapsed_counters_total",
+    help: "The number of counter intervals that have elapsed."
+  });
+
+  function testEnd() {
+    t.deepEqual(counter.values(), [ [ { period: '1sec' }, counterRand ] ])
+    t.end()
+  }
+
+  var x = 0
+  var interval = setInterval(function() {
+    counter.increment({
+      period: "1sec" //period is a custom label name in this case with a value of "1sec"
+    })
+    
+    if (++x == counterRand) {
+      clearInterval(interval)
+      testEnd()
+    }
+  }, 150)
+})
+
+test('gauge (random ' + gaugeRand + ')', function(t) {
+  var Prometheus = require("./lib/client")
+  var client = new Prometheus()
+
+  var gauge = client.newGauge({
+    namespace: "gauge_test",
+    name: "random_number",
+    help: "A random number we occasionally set."
+  });
+  
+  function testEnd() {
+    t.end()
+  }
+
+  var x = 0
+  var interval = setInterval(function() {
+    var gaugeNum = Math.random() * 1000
+
+    gauge.set({
+        period: "1sec"
+    }, gaugeNum);
+    
+    t.deepEqual(gauge.values(), [ [ { period: '1sec' }, gaugeNum ] ])
+
+    if (++x == gaugeRand) {
+      clearInterval(interval)
+      testEnd()
+    }
+  }, 150)
+})
+
+test('histogram (random ' + histogramRand + ')', function(t) {
+  var Prometheus = require("./lib/client")
+  var client = new Prometheus()
+  
+  var histogram = client.newHistogram({
+    namespace: "histogram_test",
+    name: "random_histogram",
+    help: "random historgram",
+  })
+
+  function testEnd() {
+    t.equal(histogram.values()[histogram.values().length-1][0].le, '+Inf')
+    t.equal(histogram.values()[histogram.values().length-1][1], histogramRand)
+    t.end()
+  }
+
+  var x = 0
+  var interval = setInterval(function() {
+    var rand = Math.random() * (0 + 10) + 1
+    histogram.observe(rand)
+
+    if (++x == histogramRand) {
+      clearInterval(interval)
+      testEnd()
+    }
+  }, 150)
+})

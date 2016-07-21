8624927b7d7e7d564408f212108013f61c568536
Erik Kristensen <erik@erikkristensen.com>
2016-07-21 12:27:26 -0600

    ensure all tests pass

--- test.js
+++ test.js
@@ -15,6 +15,32 @@ test('counter (random ' + counterRand + ')', function(t) {
   });
 
   function testEnd() {
+    t.deepEqual(counter.values(), [ [ {}, counterRand ] ])
+    t.end()
+  }
+
+  var x = 0
+  var interval = setInterval(function() {
+    counter.increment()
+    
+    if (++x == counterRand) {
+      clearInterval(interval)
+      testEnd()
+    }
+  }, 150)
+})
+
+test('counter with labels (random ' + counterRand + ')', function(t) {
+  var Prometheus = require("./lib/client")
+  var client = new Prometheus()
+
+  var counter = client.createCounter({
+    namespace: "counter_test",
+    name: "elapsed_counters_total",
+    help: "The number of counter intervals that have elapsed."
+  });
+
+  function testEnd() {
     t.deepEqual(counter.values(), [ [ { period: '1sec' }, counterRand ] ])
     t.end()
   }
@@ -33,6 +59,34 @@ test('counter (random ' + counterRand + ')', function(t) {
 })
 
 test('gauge (random ' + gaugeRand + ')', function(t) {
+  var Prometheus = require("./lib/client")
+  var client = new Prometheus()
+
+  var gauge = client.createGauge({
+    namespace: "gauge_test",
+    name: "random_number",
+    help: "A random number we occasionally set."
+  });
+  
+  function testEnd() {
+    t.end()
+  }
+
+  var x = 0
+  var interval = setInterval(function() {
+    var gaugeNum = Math.random() * 1000
+
+    gauge.set(gaugeNum)
+    t.deepEqual(gauge.values(), [ [ {}, gaugeNum ] ])
+
+    if (++x == gaugeRand) {
+      clearInterval(interval)
+      testEnd()
+    }
+  }, 150)
+})
+
+test('gauge with labels (random ' + gaugeRand + ')', function(t) {
   var Prometheus = require("./lib/client")
   var client = new Prometheus()
 
